8d2f9ae595a8b18dc9897aa9373d2542d2cfa7cf
Alejandro Soler Gayoso <asolergayoso@gmail.com>
2017-10-03 12:10:08 -0400

    creation of model

--- neural_network.py
+++ neural_network.py
@@ -4,3 +4,6 @@ import numpy
 
 from keras.models import Sequential
 
+model = Sequential()
+model.add(Dense(10, input_shape = (28*28, ), kernel_initializer='he_normal'))
+model.add(Activation('relu'))
